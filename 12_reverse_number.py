363b9282efdb96ec1e6fb2addf8c7e901b8b7efa
tofik <tofik.imscit21@gmail.com>
2025-08-06 11:40:20 +0530

    Reverse done

---
+++ 12_reverse_number.py
@@ -0,0 +1,29 @@
+# Method 1: Using Simple Iteration
+
+
+num = int(input("Enter Number : "))
+rev = 0
+while num != 0:
+    r = num % 10
+    num = num // 10
+    rev = rev * 10 + r
+print(rev)
+
+
+# Method 2: Using String Slicing
+num = input("Enter Number : ")
+print(num[::-1])
+
+
+# Method 3: Using Recursion
+def findRev(num, rev):
+    if num == 0:
+        return rev
+    r = num % 10
+    rev = rev * 10 + r
+    return findRev(num // 10, rev)
+
+
+num = int(input("Enter Number : "))
+rev = 0
+print(findRev(num, rev))
